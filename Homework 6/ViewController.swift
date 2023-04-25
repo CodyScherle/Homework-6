8c5e969e78f43e7a8be2a8e49b2986057d438031
Cody Scherle <c.scherle@setonhill.edu>
2023-04-25 13:37:46 -0400

    Initial Commit

---
+++ Homework 6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 6
+//
+//  Created by c.scherle on 4/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
