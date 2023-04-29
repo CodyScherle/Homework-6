8587bd730a439bd038b7fca91fd28374e2ff60c0
Cody Scherle <c.scherle@setonhill.edu>
2023-04-28 20:04:24 -0400

    Homework 6

--- Homework 6/ViewController.swift
+++ Homework 6/ViewController.swift
@@ -3,15 +3,56 @@
 //  Homework 6
 //
 //  Created by c.scherle on 4/25/23.
-//
+// 56.565106, 9.030866
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var distanceLabel: UILabel!
+    let locMan: CLLocationManager = CLLocationManager()
+    var startLocation: CLLocation!
+    
+    let kskiveLatitude: CLLocationDegrees = 56.565106
+    let kskiveLongitude: CLLocationDegrees = 9.030866
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let newLocation: CLLocation=locations[0]
+        NSLog("Something is happening")
+        
+        
+        if newLocation.horizontalAccuracy >= 0 {
+            let skive:CLLocation = CLLocation(latitude: kskiveLatitude,longitude: kskiveLongitude)
+            let delta:CLLocationDistance = skive.distance(from: newLocation)
+            let miles: Double = (delta * 0.00621371) + 0.5
+            if miles < 3 {
+                locMan.stopUpdatingLocation()
+                distanceLabel.text = "Welcome to Skive!"
+        } else {
+            let commaDelimited: NumberFormatter = NumberFormatter()
+            commaDelimited.numberStyle = NumberFormatter.Style.decimal
+            distanceLabel.text=commaDelimited.string(from: NSNumber(value: miles))!+" miles to Skive"
+        }
+        }
+           
+        
+        
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        locMan.delegate = self
+        locMan.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+        locMan.distanceFilter = 1609;
+        locMan.requestWhenInUseAuthorization()
+        locMan.startUpdatingLocation()
+        startLocation = nil
     }
 
 
