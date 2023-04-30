5e3ff0aabe6e6219d443aa139d342f46cd02ffba
Cody Scherle <c.scherle@setonhill.edu>
2023-04-29 23:49:55 -0400

    Homework 8

--- Homework 6/ViewController.swift
+++ Homework 6/ViewController.swift
@@ -10,6 +10,14 @@ import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
 
+ 
+    @IBAction func openSite(_ sender: Any) {
+        if let url = URL (string: "https://www.tripadvisor.com",};  { UIApplication.shared.open(url, option: [:])
+            
+        }
+    
+    }
+    
     @IBOutlet weak var distanceLabel: UILabel!
     let locMan: CLLocationManager = CLLocationManager()
     var startLocation: CLLocation!
@@ -17,31 +25,26 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     let kskiveLatitude: CLLocationDegrees = 56.565106
     let kskiveLongitude: CLLocationDegrees = 9.030866
     
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let newLocation: CLLocation=locations[0]
-        NSLog("Something is happening")
-        
-        
-        if newLocation.horizontalAccuracy >= 0 {
-            let skive:CLLocation = CLLocation(latitude: kskiveLatitude,longitude: kskiveLongitude)
-            let delta:CLLocationDistance = skive.distance(from: newLocation)
-            let miles: Double = (delta * 0.00621371) + 0.5
-            if miles < 3 {
-                locMan.stopUpdatingLocation()
-                distanceLabel.text = "Welcome to Skive!"
-        } else {
-            let commaDelimited: NumberFormatter = NumberFormatter()
-            commaDelimited.numberStyle = NumberFormatter.Style.decimal
-            distanceLabel.text=commaDelimited.string(from: NSNumber(value: miles))!+" miles to Skive"
+                          func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            let newLocation: CLLocation=locations[0]
+            NSLog("Something is happening")
+            
+            
+            if newLocation.horizontalAccuracy >= 0 {
+                let skive:CLLocation = CLLocation(latitude: kskiveLatitude,longitude: kskiveLongitude)
+                let delta:CLLocationDistance = skive.distance(from: newLocation)
+                let miles: Double = (delta * 0.00621371) + 0.5
+                if miles < 3 {
+                    locMan.stopUpdatingLocation()
+                    distanceLabel.text = "Welcome to Skive!"
+                } else {
+                    let commaDelimited: NumberFormatter = NumberFormatter()
+                    commaDelimited.numberStyle = NumberFormatter.Style.decimal
+                    distanceLabel.text=commaDelimited.string(from: NSNumber(value: miles))!+" miles to Skive"
+                }
+            }
+            
         }
-        }
-           
-        
-        
-    }
-    
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,8 +56,17 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locMan.requestWhenInUseAuthorization()
         locMan.startUpdatingLocation()
         startLocation = nil
+            
+            let myURL = URL(string: "https://www.tripadvisor.com/")
+            let myRequest = URLRequest (url: myURL!)
+            webView.load(myRequest)
+            
+            
+            
+            
+            
     }
 
 
-}
+
 
